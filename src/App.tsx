e923233874fe33f110c9cc46cff468afee40544c
Sungho Park <parksungho320@gmail.com>
2024-02-18 19:41:30 +0800

    update intro message

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import './styles.css'
 export const App = () => {
   return (
     <div>
-      <h1>
-        Hi React Typescript Webpack - {process.env.NODE_ENV} {process.env.name}
-      </h1>
+      <h1>Hi React Typescript. This is money cracker!</h1>
       <ClickCounter />
     </div>
   )
