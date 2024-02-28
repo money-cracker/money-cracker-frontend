7e017d1f63b80ff50ad1739fb7d3f82639d22893
Sungho Park <parksungho320@gmail.com>
2024-02-28 23:03:38 +0800

    test

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,9 @@
-import { ClickCounter } from './components/ClickCounter'
 import '../index.css'
 
 export const App = () => {
   return (
     <div>
       <h1>Hi This is money cracker!</h1>
-      <ClickCounter />
     </div>
   )
 }
