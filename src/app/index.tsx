ce66eb01ced7e0bb5f4f8eaf5223b933d051f6b1
Sungho Park <parksungho320@gmail.com>
2024-02-28 21:07:59 +0800

    message update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import '../index.css'
 export const App = () => {
   return (
     <div>
-      <h1>Hi React Typescript. This is money cracker!</h1>
+      <h1>Hi This is money cracker!</h1>
       <ClickCounter />
     </div>
   )
