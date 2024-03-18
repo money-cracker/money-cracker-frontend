bcc93c7638090e678438140ce37ac9d444ef1948
Sungho Park <parksungho320@gmail.com>
2024-03-18 20:59:12 +0800

    add deploy

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const HomePage = () => {
-  return <div>1.0.1 Home Page test deploy</div>
+  return <div>1.0.2 Home Page test deploy successful!!</div>
 }
 
 export default HomePage
