10a764acfd885bd2cbee01c75d08cbc601b38b1b
Sungho Park <parksungho320@gmail.com>
2024-03-18 22:25:14 +0800

    update welcome message

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const HomePage = () => {
-  return <div>1.0.2 Home Page test deploy successful!!</div>
+  return <div>This is Money Cracker</div>
 }
 
 export default HomePage
