e3f0f304c1558f00417ba5c7cf2bae81b1752efd
Sungho Park <parksungho320@gmail.com>
2024-04-08 23:10:23 +0800

    add webpack setting for 404 pages

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -35,6 +35,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '..', './build'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebpackPlugin({
